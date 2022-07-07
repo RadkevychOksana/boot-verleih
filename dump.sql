70dfa2dcb9be890c2ac4bd0a08fe1d4f7a84a2b9
Radkevych Oksana <radkevych.oksana@gmail.com>
2022-07-07 23:29:35 +0200

    Update dump.sql

--- dump.sql
+++ dump.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Erstellungszeit: 07. Jul 2022 um 16:18
--- Server-Version: 10.4.24-MariaDB
--- PHP-Version: 8.1.6
+-- Хост: localhost:8889
+-- Час створення: Лип 07 2022 р., 23:28
+-- Версія сервера: 5.7.34
+-- Версія PHP: 7.4.21
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,13 +18,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Datenbank: `boots_verleih`
+-- База даних: `boot_verleih`
 --
 
 -- --------------------------------------------------------
 
 --
--- Tabellenstruktur für Tabelle `boat`
+-- Структура таблиці `boat`
 --
 
 CREATE TABLE `boat` (
@@ -43,45 +43,45 @@ CREATE TABLE `boat` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Daten für Tabelle `boat`
+-- Дамп даних таблиці `boat`
 --
 
 INSERT INTO `boat` (`id`, `name`, `price`, `category_slug`, `image_1`, `image_2`, `image_3`, `length`, `width`, `seats`, `baggage`, `weight`) VALUES
-(1, 'Canoe Spark 450', 200, 'canoe', 'img/boats/canoe_duo_1.jpg', 'img/boats/canoe_duo_2.jpg', 'img/boats/canoe_duo_3.jpg', 450, 83, 2, 1, '22'),
-(2, 'Canoe Boat 380K-2', 500, 'canoe', 'img/boats/canoe_saranac_1.jpg', 'img/boats/canoe_saranac_2.jpg', 'img/boats/canoe_saranac_3.jpg', 380, 98, 2, 1, '17'),
-(3, 'Helena-Kayaks Duo', 500, 'kayaky', 'img/boats/kayak-duo-1.jpg', 'img/boats/kayak-duo-2.jpg', 'img/boats/kayak-duo-3.jpg', 440, 80, 2, 2, '32'),
-(4, 'Helena-Kayaks Trio', 300, 'kayaky', 'img/boats/kayak-trio-1.jpg', 'img/boats/kayak-trio-2.jpg', 'img/boats/kayak-trio-3.jpg', 445, 85, 3, 1, '33'),
-(5, 'Red River Sport 245', 450, 'rafty', 'img/boats/raft-one-1.jpg', '', '', 245, 95, 1, 1, '8'),
-(6, 'Fiord RM440N53', 250, 'rafty', 'img/boats/raft-two-1.jpg', '', '', 440, 190, 2, 1, '42');
+(1, 'Canoe Spark 450', 200, 'canoe', 'assets/img/boats/canoe_duo_1.jpg', 'assets/img/boats/canoe_duo_2.jpg', 'assets/img/boats/canoe_duo_3.jpg', 450, 83, 2, 1, '22'),
+(2, 'Canoe Boat 380K-2', 500, 'canoe', 'assets/img/boats/canoe_saranac_1.jpg', 'assets/img/boats/canoe_saranac_2.jpg', 'assets/img/boats/canoe_saranac_3.jpg', 380, 98, 2, 1, '17'),
+(3, 'Helena-Kayaks Duo', 500, 'kayaky', 'assets/img/boats/kayak-duo-1.jpg', 'assets/img/boats/kayak-duo-2.jpg', 'assets/img/boats/kayak-duo-3.jpg', 440, 80, 2, 2, '32'),
+(4, 'Helena-Kayaks Trio', 300, 'kayaky', 'assets/img/boats/kayak-trio-1.jpg', 'assets/img/boats/kayak-trio-2.jpg', 'assets/img/boats/kayak-trio-3.jpg', 445, 85, 3, 1, '33'),
+(5, 'Red River Sport 245', 450, 'rafty', 'assets/img/boats/raft-one-1.jpg', '', '', 245, 95, 1, 1, '8'),
+(6, 'Fiord RM440N53', 250, 'rafty', 'assets/img/boats/raft-two-1.jpg', '', '', 440, 190, 2, 1, '42');
 
 -- --------------------------------------------------------
 
 --
--- Tabellenstruktur für Tabelle `boot_categorie`
+-- Структура таблиці `boot_categorie`
 --
 
 CREATE TABLE `boot_categorie` (
   `id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
   `slug` varchar(255) NOT NULL,
-  `active` tinyint(1) NOT NULL DEFAULT 1,
+  `active` tinyint(1) NOT NULL DEFAULT '1',
   `carousel_image` varchar(255) NOT NULL,
   `price_image` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Daten für Tabelle `boot_categorie`
+-- Дамп даних таблиці `boot_categorie`
 --
 
 INSERT INTO `boot_categorie` (`id`, `name`, `slug`, `active`, `carousel_image`, `price_image`) VALUES
-(1, 'Kanu', 'canoe', 1, 'img/carousel-canoe.jpg', 'img/price-canoe.jpg'),
-(2, 'Kayaks', 'kayaky', 1, 'img/carousel-kayak.jpg', 'img/price-kayak.jpg'),
-(3, 'Flösse', 'rafty', 1, 'img/carousel-raft.jpg', 'img/price-raft.jpg');
+(1, 'Kanu', 'canoe', 1, 'assets/img/carousel-canoe.jpg', 'assets/img/price-canoe.jpg'),
+(2, 'Kayaks', 'kayaky', 1, 'assets/img/carousel-kayak.jpg', 'assets/img/price-kayak.jpg'),
+(3, 'Flösse', 'rafty', 1, 'assets/img/carousel-raft.jpg', 'assets/img/price-raft.jpg');
 
 -- --------------------------------------------------------
 
 --
--- Tabellenstruktur für Tabelle `buchung`
+-- Структура таблиці `buchung`
 --
 
 CREATE TABLE `buchung` (
@@ -94,18 +94,17 @@ CREATE TABLE `buchung` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Daten für Tabelle `buchung`
+-- Дамп даних таблиці `buchung`
 --
 
 INSERT INTO `buchung` (`id`, `boot_id`, `name`, `email`, `date`, `time`) VALUES
-(22, 1, 'Oksana Radkevych', 'radoks@gmail.com', '07/17/2022', '10:00 AM'),
-(23, 6, 'Oksana Radkevych', 'oksana.radkevych@wagner-ecommerce.group', '07/28/2022', '12:20 PM'),
-(24, 5, 'Oksana radkevych', 'radoks@gmail.com', '07/14/2022', '2:57 PM');
+(26, 3, 'Oksana Radkevych', 'radoks5@gmail.com', '07/20/2022', '11:15 AM'),
+(27, 5, 'Oksana Radkevych', 'radoks5@gmail.com', '07/21/2022', '11:12 AM');
 
 -- --------------------------------------------------------
 
 --
--- Tabellenstruktur für Tabelle `users`
+-- Структура таблиці `users`
 --
 
 CREATE TABLE `users` (
@@ -117,72 +116,74 @@ CREATE TABLE `users` (
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 --
--- Daten für Tabelle `users`
+-- Дамп даних таблиці `users`
 --
 
 INSERT INTO `users` (`id`, `first_name`, `last_name`, `email_form`, `password_form`) VALUES
-(7, 'Oksana ', 'Radkevych', 'radoks@gmail.com', '16d5cd89af204ad9f8c31e69b5f758f1'),
-(2, 'Oksana', 'Radkevych', 'radoks@gmail.com', '87456123'),
-(3, 'Oksana', 'Radkevych', 'radoks@gmail.com', '12345678'),
-(4, 'Oksana', 'Radkevych', 'radoks@gmail.com', '12345678'),
-(5, 'Oksana', 'Radkevych', 'radoks@gmail.com', '12345678'),
-(6, 'Oksana', 'Radkevych', 'radoks@gmail.com', 'cbcb66b55f2f95b4b5ee7d0b3ca127b2');
+(12, 'hhh', 'hhh', 'ff@ss.ss', '3ea3ec8ef84135148384cd177049aa6d'),
+(11, 'marine.senko@gmail.com', 'marine.senko@gmail.com', 'marine.senko@gmail.com', '79eecd52f6432626b0c809f0f97f5c63'),
+(10, 'Oksana', 'Radkevych', 'radoks@gmail.com', '1d7579cec04422de22d6b9794be39b3c'),
+(9, 'Marina ', 'Senko', 'marine.senko@gmail.com', '630e3987799885fc2a1758ed39e76293'),
+(13, 'Oksana', 'Radkevych', 'marine.senko2@gmail.com', '3ea3ec8ef84135148384cd177049aa6d'),
+(14, 'marina', 'senko', 'marina.senko@test.com', '3ea3ec8ef84135148384cd177049aa6d'),
+(15, 'Oksana', 'Radkevych', 'radoks3@gmail.com', '3ea3ec8ef84135148384cd177049aa6d'),
+(16, 'Oksana', 'Radkevych', 'radoks5@gmail.com', '3ea3ec8ef84135148384cd177049aa6d');
 
 --
--- Indizes der exportierten Tabellen
+-- Індекси збережених таблиць
 --
 
 --
--- Indizes für die Tabelle `boat`
+-- Індекси таблиці `boat`
 --
 ALTER TABLE `boat`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indizes für die Tabelle `boot_categorie`
+-- Індекси таблиці `boot_categorie`
 --
 ALTER TABLE `boot_categorie`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indizes für die Tabelle `buchung`
+-- Індекси таблиці `buchung`
 --
 ALTER TABLE `buchung`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indizes für die Tabelle `users`
+-- Індекси таблиці `users`
 --
 ALTER TABLE `users`
   ADD UNIQUE KEY `id` (`id`);
 
 --
--- AUTO_INCREMENT für exportierte Tabellen
+-- AUTO_INCREMENT для збережених таблиць
 --
 
 --
--- AUTO_INCREMENT für Tabelle `boat`
+-- AUTO_INCREMENT для таблиці `boat`
 --
 ALTER TABLE `boat`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
--- AUTO_INCREMENT für Tabelle `boot_categorie`
+-- AUTO_INCREMENT для таблиці `boot_categorie`
 --
 ALTER TABLE `boot_categorie`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- AUTO_INCREMENT für Tabelle `buchung`
+-- AUTO_INCREMENT для таблиці `buchung`
 --
 ALTER TABLE `buchung`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 
 --
--- AUTO_INCREMENT für Tabelle `users`
+-- AUTO_INCREMENT для таблиці `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
